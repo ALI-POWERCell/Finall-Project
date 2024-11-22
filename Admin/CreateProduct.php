f8c35abff999f3a1205732aeb233db8bac5bf63a
Meraj-Parhizkari <mearjp8@gmail.com>
2024-11-22 23:36:48 +0330

    Add Page Create Product

---
+++ Admin/CreateProduct.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="">
+        <label for="name">name:</label><br>
+        <input type="text" id="name" name="name" value=""><br>
+
+        <label for="price">price:</label><br>
+        <input type="text" id="price" name="price" value=""><br>
+
+        <label for="Seller ">Seller:</label><br>
+        <input type="text" id="Seller" name="Seller" value=""><br>
+
+        <label for="guarantee">guarantee:</label><br>
+        <input type="text" id="guarantee" name="guarantee" value=""><br>
+
+        <label for="count">count:</label><br>
+        <input type="text" id="count" name="count" value=""><br>
+
+        <label for="OFF-Price">OFF-Price:</label><br>
+        <input type="text" id="OFF-Price" name="OFF-Price" value=""><br><br>
+        <input type="submit " value="Submit ">
+    </form>
+</body>
+
+</html>
